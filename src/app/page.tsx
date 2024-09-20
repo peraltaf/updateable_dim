32069db3d0162dbf31bd00556c12e8ff927a5356
Felman Peralta <fperalta@redventures.com>
2024-09-20 07:38:51 -0700

    adding vercel insights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,12 @@ import Header from './components/Header';
 import MainApp from './components/Main';
 import { useLogin } from './context/store';
 import SimpleLogin from './components/SimpleLogin';
+import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
+
+inject();
+injectSpeedInsights();
 
 export default function FullFeaturedCrudGrid() {
   const { info } = useLogin();
