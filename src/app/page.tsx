3b3697d7c49bedd2760dd9f00003035dd1c5d856
Felman Peralta <fperalta@redventures.com>
2024-09-26 12:59:34 -0700

    updating title and description, trying to get analytics working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,7 @@ import Header from './components/Header';
 import MainApp from './components/Main';
 import { useLogin } from './context/store';
 import SimpleLogin from './components/SimpleLogin';
-import { inject } from '@vercel/analytics';
-import { injectSpeedInsights } from '@vercel/speed-insights';
 
-
-inject();
-injectSpeedInsights();
 
 export default function FullFeaturedCrudGrid() {
   const { info } = useLogin();
